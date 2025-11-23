f02aa436fbc859d12daa11073ce38cf48704a5d5
briancarreras91 <briancarreras91@gmail.com>
2025-11-23 18:54:08 -0300

    Agrego NavBar.jsx y actualizo App.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import "bootstrap/dist/css/bootstrap.min.css"; // ✅ estilos globales desde App
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
